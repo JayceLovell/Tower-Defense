7a6a7538d9a920ad9abc3c5559af25cc8c3ac75d
Andre Passos <stepsup@outlook.com>
2018-11-20 17:17:29 -0500

    Main Menu, Level 1, Navmesh, State Machine

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Enemy))]
+public class EnemyMovement : MonoBehaviour {
+
+	private Transform target;
+	private int wavePointIndex = 0;
+	private Enemy enemy;
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+	{
+		enemy = GetComponent<Enemy> ();
+		target = Waypoints.points [0];
+        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+
+    }
+
+	void Update ()
+	{
+		//Debug.Log ("Target: " + target.ToString ());
+		//Debug.Log ("Transform: " + transform.ToString ());
+		//Vector3 dir = target.position - transform.position;
+        //transform.Translate (dir.normalized * enemy.speed * Time.deltaTime, Space.World);
+        navMeshAgent.SetDestination(target.position);
+
+		if (Vector3.Distance (transform.position, target.position) <= 0.5f)
+		{
+			GetNextWayPoint ();
+		}
+
+		enemy.speed = enemy.startSpeed;
+	}
+
+	void GetNextWayPoint()
+	{
+        Debug.Log("Getting next waypoint"); 
+		if(wavePointIndex >= Waypoints.points.Length -1)
+		{
+			EndPath ();
+			return;
+		}
+		wavePointIndex++;
+		target = Waypoints.points [wavePointIndex];
+        //
+	}
+
+	void EndPath() 
+	{
+		PlayerStats.Lives -= 1;
+		Destroy (gameObject);
+
+		WaveSpawner.EnemiesAlive--;
+	}
+}
